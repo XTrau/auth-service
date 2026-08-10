f0ae7f3201ece7c84a2970141a69078c8f23fd5b
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2026-08-10 20:58:25 +0500

    add: domain for auth

---
+++ internal/domain/auth.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"time"
+)
+
+type Hasher interface {
+	Hash(password string) (string, error)
+	Compare(hash, password string) bool
+}
+
+type TokenPayload struct {
+	Subject  UserID `json:"id"`
+	Username string `json:"username"`
+}
+
+type TokenPair struct {
+	Access  string
+	Refresh string
+}
+
+type TokenEncoder interface {
+	Encode(payload TokenPayload, tokenType string, expire time.Duration) (string, error)
+}
+
+type TokenGenerator interface {
+	Generate(payload TokenPayload) (TokenPair, error)
+}
+
+type TokenVerifier interface {
+	Verify(token string) error
+}
+
+type TokenDecoder interface {
+	Decode(token string, tokenType string) (TokenPayload, error)
+}

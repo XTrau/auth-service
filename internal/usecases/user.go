e9f98c88c038c6273469624b9ee569744ec72876
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2026-08-11 01:45:42 +0500

    add: get user usecase

---
+++ internal/usecases/user.go
@@ -0,0 +1,27 @@
+package usecases
+
+import (
+	authjwt "github.com/XTrau/auth-service/internal/auth/jwt"
+	"github.com/XTrau/auth-service/internal/domain"
+)
+
+type GetUserUseCase struct {
+	tokenDecoder   domain.TokenDecoder
+	userRepository domain.UserRepository
+}
+
+func NewGetUserUseCase(decoder domain.TokenDecoder, userRepository domain.UserRepository) *GetUserUseCase {
+	return &GetUserUseCase{
+		tokenDecoder:   decoder,
+		userRepository: userRepository,
+	}
+}
+
+func (uc *GetUserUseCase) Execute(accessToken string) (*domain.User, error) {
+	payload, err := uc.tokenDecoder.Decode(accessToken, authjwt.AccessType)
+	if err != nil {
+		return nil, err
+	}
+
+	return uc.userRepository.GetByID(payload.Subject)
+}

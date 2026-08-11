02aff27d454786743a30b9714f531e3a3917a1f5
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2026-08-11 12:30:19 +0500

    add: migration files
    first migration for create user table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    date_of_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_users_username ON users USING HASH (username);

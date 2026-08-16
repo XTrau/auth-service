ea796ed09293c50e3f200556240766b10346b510
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2026-08-17 00:48:16 +0500

    fix: unique username in migrations

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     date_of_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

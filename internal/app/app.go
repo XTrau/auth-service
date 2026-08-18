d8d266730d1a7ad7c4c88d9fab99d2b7b55293f8
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2026-08-19 00:42:24 +0500

    add: таймауты для сервера

--- internal/app/app.go
+++ internal/app/app.go
@@ -72,6 +72,11 @@ func Run() error {
 	server := http.Server{
 		Addr:    serverAddr,
 		Handler: mux,
+
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
 	}
 
 	// Запуск сервера
